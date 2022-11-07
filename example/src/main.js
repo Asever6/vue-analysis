1c8ded4f4df6ea3316e8aa8e225e4af3ea724a44
zhangyongsheng <zhangyongsheng@niaoqiao.com>
2022-11-07 18:13:13 +0800

    修改

--- example/src/main.js
+++ example/src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+import Vue from "vue/dist/vue.esm.js";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
+var app = new Vue({
+  el: "#app",
+  mounted() {
+    console.log(this.message);
+  },
+  data() {
+    return {
+      message: "Hello Vue",
+    };
+  },
+});
